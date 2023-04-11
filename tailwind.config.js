e988ab7e2fd30da419f36daf369e9e4a4affd685
AliReza <alirezadavodiofficial@gmail.com>
2023-04-11 18:35:28 +0330

    added tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,106 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blackish: "#131822",
+        grayish: "#D8DADC",
+      },
+
+      spacing: {
+        "9/16": "56.25%",
+        "3/4": "75%",
+        "1/1": "100%",
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        Poppins: ["Poppins", "sans-serif"],
+      },
+      fontSize: {
+        xs: "0.75rem",
+        sm: "0.875rem",
+        base: "1rem",
+        lg: "1.125rem",
+        xl: "1.25rem",
+        "2xl": "1.5rem",
+        "3xl": "2rem",
+        "4xl": "2.625rem",
+        "5xl": "3.25rem",
+        "6xl": "5.5rem",
+      },
+      inset: {
+        "1/2": "50%",
+        full: "100%",
+      },
+      letterSpacing: {
+        tighter: "-0.02em",
+        tight: "-0.01em",
+        normal: "0",
+        wide: "0.01em",
+        wider: "0.02em",
+        widest: "0.4em",
+      },
+      lineHeight: {
+        none: "1",
+        tighter: "1.125",
+        tight: "1.25",
+        snug: "1.375",
+        normal: "1.5",
+        relaxed: "1.625",
+        loose: "2",
+        3: ".75rem",
+        4: "1rem",
+        5: "1.2rem",
+        6: "1.5rem",
+        7: "1.75rem",
+        8: "2rem",
+        9: "2.25rem",
+        10: "2.5rem",
+      },
+      minWidth: {
+        10: "2.5rem",
+        48: "12rem",
+        56: "14rem",
+        120: "30rem",
+      },
+      minHeight: {
+        10: "2.5rem",
+        20: "5rem",
+        24: "6rem",
+        32: "8rem",
+        48: "12rem",
+        56: "14rem",
+        120: "30rem",
+      },
+      opacity: {
+        90: "0.9",
+      },
+      scale: {
+        98: ".98",
+      },
+      animation: {
+        float: "float 3s ease-in-out infinite",
+      },
+      keyframes: {
+        float: {
+          "0%, 100%": { transform: "translateY(0)" },
+          "50%": { transform: "translateY(-5%)" },
+        },
+      },
+      zIndex: {
+        "-1": "-1",
+      },
+    },
+  },
+  variants: {
+    backgroundColor: ["responsive", "hover", "focus", "group-hover"],
+    textColor: ["responsive", "hover", "focus", "group-hover"],
+    translate: ["responsive", "hover", "focus", "group-hover"],
+    boxShadow: ["responsive", "hover", "focus", "focus-within"],
+    opacity: ["responsive", "hover", "focus", "group-hover"],
+  },
+  corePlugins: {
+    preflight: true /* the plugin extends Preflight so make sure it is not disabled */,
   },
   plugins: [],
 };
