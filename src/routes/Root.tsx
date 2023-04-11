6ec6e2e97e8a1673ed8d02e27747d3bf38236fc8
AliReza <alirezadavodiofficial@gmail.com>
2023-04-11 18:36:32 +0330

    added root of routes

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom";
 export const Root = () => {
   return (
     <div className="">
-      <main className="w-full h-screen bg-red-600">
+      <main className="">
         <div className="content">
           <Outlet />
         </div>
