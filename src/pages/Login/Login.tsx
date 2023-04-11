d9b6f1c58329bce61e2f3a00cedbee841029c65c
AliReza <alirezadavodiofficial@gmail.com>
2023-04-11 18:36:52 +0330

    added login page

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,7 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+import Input from "../../components/Input/Input";
+import { Link } from "react-router-dom";
+import Button from "../../components/Buttons/Button";
+import OpenEyeIcon from "../../assets/icons/OpenEye";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [showPass, setShowPass] = useState(false);
+
+  const showPassHandler = () => setShowPass((pre) => !pre);
+
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-screen gap-2 p-6">
+      <h1 className="text-4xl font-extrabold text-center capitalize text-blackish font-Poppins">
+        Login to site
+      </h1>
+      <p className="text-[#131822b3] text-center">
+        Please enter your username and password
+      </p>
+
+      <form action="" className="w-full">
+        <Input
+          label="Email address"
+          name="email"
+          type="email"
+          id="email"
+          placeholder="Enter your email"
+        />
+        <Input
+          label="Password"
+          name="pass"
+          type={showPass ? "text" : "password"}
+          id="pass"
+          placeholder="Enter your password"
+        >
+          <div className="pr-4 cursor-pointer" onClick={showPassHandler}>
+            <OpenEyeIcon />
+          </div>
+        </Input>
+
+        <Link
+          to="#"
+          className="block mt-4 text-sm text-right select-none hover:underline"
+        >
+          Forgot password?
+        </Link>
+
+        <div className="mt-7">
+          <Button subject="Log in" />
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
