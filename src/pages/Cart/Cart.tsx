8fe117c356620ccf69c5ace60df1c338b612f742
AliReza <alirezadavodiofficial@gmail.com>
2023-04-13 05:25:37 +0330

    added cart page

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,7 +1,43 @@
 import React from "react";
+import Card from "../../components/Card/Card";
+import Button from "../../components/Buttons/Button";
 
 const Cart = () => {
-  return <div>Cart</div>;
+  return (
+    <div className="h-screen bg-white">
+      <div className="container p-3.5 pt-20">
+        <Card layout="row" addAble />
+        <Card layout="row" addAble />
+        <Card layout="row" addAble />
+        <Card layout="row" addAble />
+        <Card layout="row" addAble />
+        <Card layout="row" addAble />
+        <Card layout="row" addAble />
+        <Card layout="row" addAble />
+        <Card layout="row" addAble />
+        <Card layout="row" addAble />
+
+        <div className="py-6 mt-6 border-t border-blackish">
+          <div className="flex items-center justify-between mb-3">
+            <h2 className="font-semibold">Product Count:</h2>
+            <span className="text-sm font-medium">3</span>
+          </div>
+
+          <div className="flex items-center justify-between mb-4">
+            <h2 className="font-semibold">Total:</h2>
+            <span className="text-sm font-medium">$1,450</span>
+          </div>
+
+          <div className="">
+            <Button
+              subject="pay via paypal"
+              extraBtnClass="capitalize font-normal"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
