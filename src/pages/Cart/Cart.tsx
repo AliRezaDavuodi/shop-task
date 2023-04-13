b4fecf0e7e5867e78a2941c49a9a80db6ba08664
AliReza <alirezadavodiofficial@gmail.com>
2023-04-13 14:11:43 +0330

    fixed cart and product container class

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,10 +1,9 @@
-import React from "react";
 import Card from "../../components/Card/Card";
 import Button from "../../components/Buttons/Button";
 
 const Cart = () => {
   return (
-    <div className="h-screen bg-white">
+    <div className="bg-white ">
       <div className="container p-3.5 pt-20">
         <Card layout="row" addAble />
         <Card layout="row" addAble />
--- src/pages/Products/Products.tsx
+++ src/pages/Products/Products.tsx
@@ -2,7 +2,7 @@ import Card from "../../components/Card/Card";
 
 const Products = () => {
   return (
-    <div className="container flex flex-col items-center justify-center gap-3 p-2.5 pt-20">
+    <div className="container h-min overflow-hidden flex flex-col items-center justify-center gap-3 p-2.5 pt-20">
       {/* Card items */}
       <Card />
       <Card />
