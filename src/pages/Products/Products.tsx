40bff6804aa2a91401e59456ab23b4e21df059c6
AliReza <alirezadavodiofficial@gmail.com>
2023-04-13 05:25:27 +0330

    added products page

--- src/pages/Products/Products.tsx
+++ src/pages/Products/Products.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+import Card from "../../components/Card/Card";
 
 const Products = () => {
-  return <div>Products</div>;
+  return (
+    <div className="container flex flex-col items-center justify-center gap-3 p-2.5 pt-20">
+      {/* Card items */}
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
 };
 
 export default Products;
