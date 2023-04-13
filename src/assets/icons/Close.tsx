6fd220486e29b3fac5c8b5b7ad4bea41e973ce1c
AliReza <alirezadavodiofficial@gmail.com>
2023-04-13 05:24:49 +0330

    added icons and images

---
+++ src/assets/icons/Close.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const CloseIcon = () => {
+  return (
+    <svg
+      width="12"
+      height="12"
+      viewBox="0 0 12 12"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M1 1L11 11M1.00002 11L6.00001 6L11 1"
+        stroke="#131822"
+        stroke-width="1.5"
+        stroke-linecap="round"
+      />
+    </svg>
+  );
+};
+
+export default CloseIcon;
---
+++ src/assets/icons/Off.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const OffIcon = () => {
+  return (
+    <svg
+      width="39"
+      height="39"
+      viewBox="0 0 39 39"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect width="39" height="39" rx="10" fill="white" />
+      <path
+        d="M26 12C28.3903 13.8273 30 16.7589 30 20C30 25.5228 25.5228 30 20 30C14.4772 30 10 25.5228 10 20C10 16.7285 11.5713 13.8244 14 12M20 10V17"
+        stroke="#131822"
+        stroke-width="1.5"
+        stroke-linecap="round"
+      />
+    </svg>
+  );
+};
+
+export default OffIcon;
