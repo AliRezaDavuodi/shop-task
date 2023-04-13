877c0c11d43ab9b6fb393419c1f8b18f4f8d5134
AliReza <alirezadavodiofficial@gmail.com>
2023-04-13 05:25:48 +0330

    added search page

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -1,7 +1,16 @@
-import React from "react";
+import Card from "../../components/Card/Card";
 
 const Search = () => {
-  return <div>Search</div>;
+  return (
+    <div className="p-3 py-20">
+      <Card layout="row" />
+      <Card layout="row" />
+      <Card layout="row" />
+      <Card layout="row" />
+      <Card layout="row" />
+      <Card layout="row" />
+    </div>
+  );
 };
 
 export default Search;
