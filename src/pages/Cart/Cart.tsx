7b2398a4431d4b3d858287abb5305a6f9e6ef264
AliReza <alirezadavodiofficial@gmail.com>
2023-04-14 14:00:43 +0330

    added cart functionality

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,36 +1,64 @@
 import Card from "../../components/Card/Card";
 import Button from "../../components/Buttons/Button";
+import { useSelector } from "react-redux";
+import { RootState } from "../../store/store";
+import { useEffect, useState } from "react";
 
 const Cart = () => {
+  const cart = useSelector((state: RootState) => state.cart.basket);
+
+  const [total, setTotal] = useState({
+    count: 0,
+    price: 0,
+  });
+
+  useEffect(() => {
+    const totalPrice = cart.reduce(
+      (acc, cur) => acc + cur.price * (cur.amount || 1),
+      0
+    );
+    const totalCount = cart.reduce((acc, cur) => acc + (cur.amount || 1), 0);
+
+    setTotal({
+      count: totalCount,
+      price: totalPrice,
+    });
+  }, [cart]);
+
   return (
     <div className="bg-white ">
       <div className="container p-3.5 pt-20">
-        <Card layout="row" addAble />
-        <Card layout="row" addAble />
-        <Card layout="row" addAble />
-        <Card layout="row" addAble />
-        <Card layout="row" addAble />
-        <Card layout="row" addAble />
-        <Card layout="row" addAble />
-        <Card layout="row" addAble />
-        <Card layout="row" addAble />
-        <Card layout="row" addAble />
+        {cart.map((item) => (
+          <Card
+            layout="row"
+            addAble
+            key={item.id}
+            desc={item.description}
+            title={item.title}
+            image={item.images[0]}
+            price={item.price}
+            id={item.id}
+            rank={item.rating}
+            amount={item.amount}
+          />
+        ))}
 
         <div className="py-6 mt-6 border-t border-blackish">
           <div className="flex items-center justify-between mb-3">
             <h2 className="font-semibold">Product Count:</h2>
-            <span className="text-sm font-medium">3</span>
+            <span className="text-sm font-medium">{total.count}</span>
           </div>
 
           <div className="flex items-center justify-between mb-4">
             <h2 className="font-semibold">Total:</h2>
-            <span className="text-sm font-medium">$1,450</span>
+            <span className="text-sm font-medium">${total.price}</span>
           </div>
 
           <div className="">
             <Button
               subject="pay via paypal"
               extraBtnClass="capitalize font-normal"
+              disabled={total.count === 0}
             />
           </div>
         </div>
--- src/store/cart.ts
+++ src/store/cart.ts
@@ -1,64 +1,66 @@
-// import { createSlice } from "@reduxjs/toolkit";
-// import { notif } from "utilities/toast";
+import { createSlice } from "@reduxjs/toolkit";
+import { productsType } from "../@types/service/products";
+import { storeCart } from "../utils/storeCart";
 
-// const INITIAL__STATE = {
-//   basket: [],
-// };
+const INITIAL__STATE = {
+  basket: [] as productsType[],
+};
 
-// const cart = createSlice({
-//   name: "cart",
-//   initialState: INITIAL__STATE,
-//   reducers: {
-//     addItemToCart(state, action) {
-//       const exsitsItem = state.basket.find(
-//         (food) => food.id === action.payload.id
-//       );
+const cart = createSlice({
+  name: "cart",
+  initialState: INITIAL__STATE,
+  reducers: {
+    addItemToCart(state, action) {
+      const existsItem = state.basket.find(
+        (food) => food.id === action.payload.id
+      );
 
-//       if (!exsitsItem) {
-//         // if food is new, push it to the basket
-//         state.basket.push({ ...action.payload, amount: 1 });
+      if (!existsItem) {
+        // if food is new, push it to the basket
+        state.basket.push({ ...action.payload, amount: 1 });
+        storeCart(state.basket);
+        return;
+      }
+      // increase its amount by one
+      existsItem.amount && existsItem.amount++;
+      storeCart(state.basket);
+    },
+    increaseItem(state, action) {
+      // find the item
+      const item = state.basket.find((food) => food.id === action.payload.id);
+      // increase its amount by one
+      item && item.amount && item.amount++;
 
-//         return;
-//       }
-//       // increase its amount by one
-//       exsitsItem.amount++;
-//     },
-//     deleteItemFromCart(state, action) {
-//       // filter the basket array
-//       state.basket = state.basket.filter(
-//         (food) => food.id !== action.payload.id
-//       );
-//     },
-//     increaseItem(state, action) {
-//       // find the item
-//       const item = state.basket.find((food) => food.id === action.payload.id);
-//       // increase its amount by one
-//       item.amount++;
-//     },
-//     decreaseItem(state, action) {
-//       // find item
-//       const item = state.basket.find((food) => food.id === action.payload.id);
-//       // chcek its amount
-//       // if it's grather than one decrease it by one
-//       if (item.amount > 1) {
-//         item.amount--;
+      storeCart(state.basket);
+    },
+    decreaseItem(state, action) {
+      // find item
+      const item = state.basket.find((food) => food.id === action.payload.id);
 
-//         return;
-//       }
-//       // if it's one delete it from cart
-//       state.basket = state.basket.filter(
-//         (food) => food.id !== action.payload.id
-//       );
-//     },
+      if (!item) return;
 
-//     replaceCartbasket(state, action) {
-//       state.basket = [...action.payload];
-//     },
-//   },
-// });
+      // check its amount
+      // if it's grater than one decrease it by one
+      if (item.amount && item.amount > 1) {
+        item.amount--;
+        storeCart(state.basket);
+        return;
+      }
+      // if it's one delete it from cart
+      state.basket = state.basket.filter(
+        (food) => food.id !== action.payload.id
+      );
 
-// export const cartActions = cart.actions;
+      storeCart(state.basket);
+    },
+    replaceCartBasket(state, action) {
+      console.log("ppp", action.payload);
+      state.basket = [...action.payload];
+      console.log("setted");
+    },
+  },
+});
 
-// export default cart.reducer;
+export const cartActions = cart.actions;
 
-export const a = {};
+export default cart.reducer;
