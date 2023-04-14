0c9e8aebd366523093a095dfdab1239c7e0753e2
AliReza <alirezadavodiofficial@gmail.com>
2023-04-14 05:30:03 +0330

    added search page functionality

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -1,14 +1,31 @@
+import { useSelector } from "react-redux";
 import Card from "../../components/Card/Card";
+import { RootState } from "../../store/store";
+import { useEffect } from "react";
 
 const Search = () => {
+  const products = useSelector(
+    (state: RootState) => state.products.filterProducts
+  );
+
+  useEffect(() => {
+    console.log({ products });
+  }, [products]);
+
   return (
     <div className="p-3 py-20">
-      <Card layout="row" />
-      <Card layout="row" />
-      <Card layout="row" />
-      <Card layout="row" />
-      <Card layout="row" />
-      <Card layout="row" />
+      {products.map((item) => (
+        <Card
+          layout="row"
+          key={item.id}
+          desc={item.description}
+          title={item.title}
+          image={item.images[0]}
+          price={item.price}
+          id={item.id}
+          rank={item.rating}
+        />
+      ))}
     </div>
   );
 };
