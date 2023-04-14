35a85dc58282dcf7d6683507a46672464ef39af8
AliReza <alirezadavodiofficial@gmail.com>
2023-04-14 14:01:02 +0330

    added product page

--- src/pages/Products/Products.tsx
+++ src/pages/Products/Products.tsx
@@ -1,31 +1,11 @@
 import { useEffect } from "react";
 import Card from "../../components/Card/Card";
-import { getProducts } from "../../service/user.service";
-import { useDispatch } from "react-redux";
-import { ProductsActions } from "../../store/products";
+
 import { useSelector } from "react-redux";
 import { RootState } from "../../store/store";
 
 const Products = () => {
-  const dispatch = useDispatch();
-
   const products = useSelector((state: RootState) => state.products.products);
-
-  useEffect(() => {
-    const getAllProducts = async () => {
-      try {
-        const data = await getProducts();
-
-        if (!data) return;
-
-        dispatch(ProductsActions.replaceCartInfo(data));
-      } catch (error) {
-        console.log({ error });
-      }
-    };
-
-    getAllProducts();
-  }, []);
 
   return (
     <div className="container h-min overflow-hidden flex flex-col items-center justify-center gap-3 p-2.5 pt-20">
