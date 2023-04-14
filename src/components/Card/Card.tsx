b62da93016636d696ff14f24764a39288ee1a435
AliReza <alirezadavodiofficial@gmail.com>
2023-04-14 05:29:01 +0330

    added dynamic card

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,24 +1,29 @@
 import React, { FC } from "react";
 import StarIcon from "../../assets/icons/Star";
-import ProductImage from "../../assets/images/productImage.png";
 import { Link } from "react-router-dom";
 
 interface CardProps {
   layout?: "row" | "col";
   addAble?: true | false;
+  title: string;
+  desc?: string;
+  rank?: number;
+  image: string;
+  price?: number;
+  id: number;
 }
 
 const Card: FC<CardProps> = (props) => {
-  const { layout, addAble } = props;
+  const { layout, addAble, desc, image, price, rank, title, id } = props;
 
   if (layout === "row") {
     return (
-      <Link to={"/products/1"}>
+      <Link to={"/products/" + id} className="w-full">
         <div className="p-3 mb-1.5 bg-white rounded-2xl card">
           <div className="flex gap-4">
             <div className="flex-1 overflow-hidden min-w-28 card__cover rounded-xl">
               <img
-                src={ProductImage}
+                src={image}
                 alt="cover"
                 className="object-cover w-full h-full"
               />
@@ -26,11 +31,11 @@ const Card: FC<CardProps> = (props) => {
             <div className="mt-2.5 card__info">
               <div className="">
                 <h2 className="flex items-center justify-between text-base font-semibold ">
-                  iPhone 9
+                  {title}
                 </h2>
 
                 <p className="mt-0.5 text-lighterBlackish first-letter:capitalize text-sm">
-                  an apple mobile which is nothing like apple
+                  {desc}
                 </p>
               </div>
 
@@ -40,12 +45,12 @@ const Card: FC<CardProps> = (props) => {
                     <div className="star">
                       <StarIcon />
                     </div>
-                    4.69
+                    {rank}
                   </div>
                 )}
 
                 <h2 className="flex items-center justify-between text-base font-semibold">
-                  $549
+                  ${price}
                 </h2>
                 {addAble && (
                   <div className="flex items-center gap-2.5 actions__btn">
@@ -67,30 +72,30 @@ const Card: FC<CardProps> = (props) => {
   }
 
   return (
-    <Link to={"/products/1"}>
+    <Link to={"/products/" + id} className="w-full">
       <div className="p-1 bg-white rounded-2xl card">
-        <div className="overflow-hidden card__cover rounded-xl">
+        <div className="overflow-hidden h-48 card__cover rounded-xl bg-grayish">
           <img
-            src={ProductImage}
+            src={image}
             alt="cover"
-            className="object-cover w-full h-full"
+            className="object-contain w-full h-full"
           />
         </div>
         <div className="mt-2.5 card__info">
           <h2 className="flex items-center justify-between text-base ">
-            <span className="font-semibold">iPhone 9</span>
-            <span className="font-medium">$549</span>
+            <span className="font-semibold">{title}</span>
+            <span className="font-medium">${price}</span>
           </h2>
 
           <p className="mt-0.5 text-lighterBlackish first-letter:capitalize text-sm">
-            an apple mobile which is nothing like apple
+            {desc}
           </p>
 
           <div className="flex items-center gap-1 mt-3.5 mb-2.5 text-sm">
             <div className="star">
               <StarIcon />
             </div>
-            4.69
+            {rank}
           </div>
         </div>
       </div>
